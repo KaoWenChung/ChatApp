6ec071841c47b5fe431a3873c9233b08e49e2e11
wyn <k022298@gmail.com>
2023-02-25 11:57:16 +0000

    Defined RightFirstMessageTableViewCell

---
+++ ChatApp/Cell/RightFirstMessageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RightFirstMessageTableViewCell.swift
+//  ChatApp
+//
+//  Created by wyn on 2023/2/24.
+//
+
+import UIKit
+
+class RightFirstMessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak private var messageTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Message"
@@ -15,8 +16,19 @@ class ViewController: UIViewController {
         appearance.backgroundColor = .systemCyan
         appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        messageTableView.register(UINib(nibName: "RightFirstMessageTableViewCell", bundle: nil), forCellReuseIdentifier: "RightFirstMessageTableViewCell")
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RightFirstMessageTableViewCell") as? RightFirstMessageTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+    
+    
+}
