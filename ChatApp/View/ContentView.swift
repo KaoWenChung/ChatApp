1fe997c44fcce8f18588208f9e9a6a2da2dae7e5
wyn <k022298@gmail.com>
2023-04-03 15:36:33 +0100

    Modified ContentView the content of body and add/ remove notification

--- ChatApp/View/ContentView.swift
+++ ChatApp/View/ContentView.swift
@@ -20,9 +20,76 @@ struct ContentView: View {
             ZStack {
                 VStack {
                     if state.loggedIn && userID != nil {
+                        LoggedInView(userID: $userID)
+                            .environment(\.realmConfiguration, app.currentUser!
+                                .configuration(partitionValue: "user=\(userID ?? "Unknown")"))
+                    } else {
+                        LoginView(userID: $userID)
+                    }
+                    Spacer()
+                    if let error = state.error {
+                        Text("Error: \(error)")
+                            .foregroundColor(.red)
+                    }
+                    if state.busyCount > 0 {
                     }
                 }
             }
+            .currentDeviceNavigationViewStyle(alwaysStacked: !state.loggedIn)
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                if shouldRemindOnlineUser {
+                    addNotification(timeInHours: Int(onlineUserReminderHours))
+                }
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                clearNotifications()
+            }
+        }
+    }
+
+    func addNotification(timeInHours: Int) {
+        let center = UNUserNotificationCenter.current()
+
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Still logged in"
+            let unit = "\(onlineUserReminderHours) \(onlineUserReminderHours == 1 ? "hour" : "hours")"
+            content.subtitle = "You've been offline in the background for" + unit
+            content.sound = .default
+
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: onlineUserReminderHours * 3600,
+                                                            repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString,
+                                                content: content,
+                                                trigger: trigger)
+            center.add(request)
+        }
+        center.getNotificationSettings { setting in
+            if setting.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, _ in
+                    if success {
+                        addRequest()
+                    }
+                }
+            }
+        }
+    }
+
+    func clearNotifications() {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllDeliveredNotifications()
+        center.removeAllPendingNotificationRequests()
+    }
+}
+
+extension View {
+    public func currentDeviceNavigationViewStyle(alwaysStacked: Bool) -> AnyView {
+        if UIDevice.current.userInterfaceIdiom == .pad && !alwaysStacked {
+            return AnyView(self.navigationViewStyle(DefaultNavigationViewStyle()))
+        } else {
+            return AnyView(self.navigationViewStyle(StackNavigationViewStyle()))
         }
     }
 }
