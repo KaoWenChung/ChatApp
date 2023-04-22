680a2108ec9d0b2ac171ca99641127acc5da94ed
wyn <k022298@gmail.com>
2023-04-22 22:46:24 +0100

    Defined the content of MapView

--- ChatApp/View/Components/Maps/MapView.swift
+++ ChatApp/View/Components/Maps/MapView.swift
@@ -9,12 +9,43 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @Environment(\.presentationMode) var presentationMode
+
     let location: CLLocationCoordinate2D
     let annotationItems: [MyAnnotationItem]
 
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: MapDefault.latitude,
+                                                                                  longitude: MapDefault.longitude),
+                                                   span: MKCoordinateSpan(latitudeDelta: MapDefault.zoomedOut,
+                                                                          longitudeDelta: MapDefault.zoomedOut))
+
+    private enum MapDefault {
+        static let latitude: CGFloat = 51.507
+        static let longitude: CGFloat = -0.127
+        static let zoomedOut: CGFloat = 2.0
+        static let zoomedIn: CGFloat = 0.01
+    }
+
     var body: some View {
-        // TODO: MapView
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            annotationItems: annotationItems) { item in
+            MapPin(coordinate: item.coordinate)
+        }
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            },
+                                                label: {
+                BackButton(label: "Dismiss")
+            }))
+            .onAppear(perform: setupLocation)
+    }
+    private func setupLocation() {
+        region = MKCoordinateRegion(center: location,
+                                    span: MKCoordinateSpan(latitudeDelta: MapDefault.zoomedIn,
+                                                           longitudeDelta: MapDefault.zoomedIn))
     }
 }
 
